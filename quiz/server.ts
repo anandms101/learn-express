b764dbca89eed52784ae8cbfe2afc74ee88726a5
ANAND MOHAN SINGH <anandmohan.singh101@gmail.com>
2025-02-22 22:56:22 -0500

    using types in server

--- quiz/server.ts
+++ quiz/server.ts
@@ -2,24 +2,7 @@ import  { promises as fsPromises } from 'fs';
 import path from 'path';
 import express, { Express, Request, Response, NextFunction } from 'express';
 import cors from 'cors';
-
-/**
- * A type that represents a user object
- */
-interface User {
-  id: number;
-  firstName: string;
-  lastName: string;
-  username: string;
-  email: string;
-}
-
-/**
- * A type that represents the request received by the server
- */
-interface UserRequest extends Request {
-  users?: User[];
-}
+import { User, UserRequest } from "./types";
 
 // the server application object created by the express server
 const app: Express = express();
